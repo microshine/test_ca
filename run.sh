0d7486843ab906bc87ea994a911431ad3305d209
microshine <microshine@mail.ru>
2017-06-26 19:18:00 +0300

    Update

--- run.sh
+++ run.sh
@@ -1,15 +1,36 @@
-# generate keys
-openssl genrsa -out root.key
-openssl genrsa -out ca.key
-openssl genrsa -out client.key
+mkdir src
+cd src
 
 # generate root
-openssl req -new -x509 -days 1826 -key root.key -out root.crt -config root.conf 
+mkdir root
+cd root
+openssl genrsa -out root.key
+openssl req -new -x509 -days 1826 -key root.key -out root.crt -config ../../root.conf
+mkdir database
+touch database/index.txt
+touch database/index.txt.attr
+cd ..
 
 # generate ca
-openssl req -new -key ca.key -out ca.req -config ca.conf
-openssl x509 -req -in ca.req -CA root.crt -CAkey root.key -out ca.crt -extfile ca.conf -extensions x509_extensions -CAcreateserial
+mkdir ca
+cd ca
+openssl genrsa -out cert.key
+openssl req -new -key cert.key -out cert.req -config ../../ca.conf
+# signing ca with root
+cd ../root
+openssl x509 -req -in ../ca/cert.req -CA root.crt -CAkey root.key -out ../ca/cert.crt -extfile ../../ca.conf -extensions x509_extensions -CAcreateserial
+cd ../ca
+mkdir database
+touch database/index.txt
+touch database/index.txt.attr
 
 # generate client
-openssl req -new -key client.key -out client.req -config client.conf
-openssl x509 -req -in client.req -CA ca.crt -CAkey ca.key -out client.crt -extfile client.conf -extensions x509_extensions -CAcreateserial
+mkdir certificates
+openssl genrsa -out certificates/client.key
+openssl req -new -key certificates/client.key -out certificates/client.req -config ../../client.conf
+openssl x509 -req -in certificates/client.req -CA cert.crt -CAkey cert.key -out certificates/client.crt -extfile ../../client.conf -extensions x509_extensions -CAcreateserial
+
+# Revoke certificate
+echo "Revoke certificate"
+openssl ca -revoke certificates/client.crt -config ../../ca.conf
+openssl ca -gencrl -config ../../ca.conf -out crl.pem
